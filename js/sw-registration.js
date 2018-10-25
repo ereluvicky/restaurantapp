a912fc604046e2ef4f43559f6387a98fa9e75893
ereluvicky <victoria.alao@yahoo.com>
2018-10-25 19:38:41 +0100

    fixed merge conflicts

--- js/sw-registration.js
+++ js/sw-registration.js
@@ -1,5 +1,4 @@
 if ('serviceWorker' in navigator) {
-<<<<<<< HEAD
 	window.addEventListener('load', function() {
 	  navigator.serviceWorker.register('/sw.js').then(function(registration) {
 		// Registration was successful
@@ -10,13 +9,3 @@ if ('serviceWorker' in navigator) {
 	  });
 	});
   }
-=======
-	navigator.serviceWorker.register('./sw.js')
-	.then(function() {
-		console.log('Registration worked!');
-	})
-	.catch(function() {
-		console.log('Registration failed!');
-	});
-}
->>>>>>> cf96e1f2a9a16b71971286a23ffa9118b0ec1b1a
